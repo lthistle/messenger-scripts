fba4611e84e7e488d0c0f70a78d20c0e4f964f6d
Luke Thistlethwaite <lukethistle.va@gmail.com>
2019-02-20 17:09:58 -0500

    Added support for removing images

--- antidelete.py
+++ antidelete.py
@@ -1,31 +1,52 @@
 #When a user deletes a message in a specificed group chat, this script will re-broadcast the deleted message
-
 from fbchat import Client
 from fbchat.models import *
-import getpass,string
+import getpass,time,os,requests,utils
 from collections import deque
 
 recent=deque([])
 sentMessages=dict()
+
+imagedir='temp_images/'
+if not os.path.isdir(imagedir):
+    os.mkdir(imagedir)
+
 class archiveBot(Client):
     def onMessage(self,mid,author_id,message_object,thread_id,thread_type,ts,**kwargs):
         if int(thread_id)==targetThread:
-            #message_text="".join(filter(lambda x: x in string.printable,message_object.text))
+            mid=mid[5:]
             sentMessages[mid]=message_object.text
             recent.append(mid)
             print("Added MID '{}' and text '{}' to dictionary".format(mid,message_object.text))
+            pos=0
+            for sentFile in message_object.attachments:
+                if isinstance(sentFile,ImageAttachment):
+                    fileprefix=imagedir+mid+'_'
+                    img_data=requests.get(self.fetchImageUrl(sentFile.uid)).content
+                    with open(fileprefix+str(pos)+'.jpg','wb') as handler:
+                        handler.write(img_data)
+                    pos+=1
             if len(recent)>100:
                 old_mid=recent.popleft()
                 print("Deleted MID '{}' and text '{}' from dictionary".format(old_mid,sentMessages[old_mid]))
+                for filename in os.listdir(imagedir):
+                    if filename.startswith(old_mid):
+                        os.remove(imagedir+filename)
                 del sentMessages[old_mid]
+
     def onMessageUnsent(self,mid,author_id,thread_id,thread_type,**kwargs):
         if int(thread_id)==targetThread:
+            mid=mid[5:]
             if mid in sentMessages:
                 newuser=self.fetchUserInfo(str(author_id))[str(author_id)]
                 send_msg="{} has removed the following message: '{}'".format(newuser.first_name,sentMessages[mid])
                 print(send_msg)
                 self.sendMessage(send_msg,thread_id=thread_id,thread_type=thread_type)
+                for filename in os.listdir(imagedir):
+                    if filename.startswith(mid):
+                        print(imagedir+filename)
+                        self.sendLocalImage(imagedir+filename,message=Message(),thread_id=thread_id,thread_type=thread_type)
+                        time.sleep(0.2)
 targetThread=int(input("Target Thread? "))
-email = input("Email address? ")
-client = archiveBot(email,getpass.getpass())
+client = archiveBot('','',session_cookies=utils.get_session())
 client.listen()
