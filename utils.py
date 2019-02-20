fbb9df78d1e9879b56ed94457655e04ee2cc307c
Luke Thistlethwaite <lukethistle.va@gmail.com>
2019-02-20 15:58:20 -0500

    Now returns session instead of actual client object

--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 #Commonly used methods
 from fbchat import Client
 import pickle,getpass
-def login_client():
+def get_session():
     useCookie=input("Use local session_cookie file to log in (Y/N)? ").lower()
     if useCookie=='y':
         try:
-            return Client('','',session_cookies=pickle.load(open('session_cookie','rb')))
+            return pickle.load(open('session_cookie','rb'))
         except:
-            print("Could not find session_cookie file, please log in manually.")
+            print("Could not find session_cookie file or file was corrupted, please log in manually.")
     email=input("Email address? ")
-    return Client(email,getpass.getpass())
-
-login_client().listen()
+    c = Client(email,getpass.getpass())
+    return c.getSession()
